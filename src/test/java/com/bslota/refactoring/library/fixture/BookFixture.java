1e11410ce278f18782b2f685ab350fc4748bcb75
bslota <bartlomiej.slota@gmail.com>
2020-06-04 17:36:03 +0200

    BookFixture added

---
+++ src/test/java/com/bslota/refactoring/library/fixture/BookFixture.java
@@ -0,0 +1,77 @@
+package com.bslota.refactoring.library.fixture;
+
+import com.bslota.refactoring.library.model.Book;
+
+import java.time.Instant;
+
+import static com.bslota.refactoring.library.fixture.BookFixture.BookBuilder.newBook;
+import static java.time.temporal.ChronoUnit.DAYS;
+
+/**
+ * @author bslota on 15/11/2019
+ */
+class BookFixture {
+
+    private static final int EXISTING_BOOK_ID = 2;
+    private static final int SOME_RESERVATION_PERIOD = 50;
+    private static final String DEFAULT_BOOK_TYPE = "CIRCULATING";
+
+    static Book availableBook() {
+        return newBook().build();
+    }
+
+    static Book unavailableBook() {
+        Instant reservationDate = Instant.now();
+        return newBook()
+                .withReservationDate(reservationDate)
+                .withReservationEndDate(reservationDate.plus(SOME_RESERVATION_PERIOD, DAYS))
+                .build();
+    }
+
+    static class BookBuilder {
+        private int bookId = EXISTING_BOOK_ID;
+        private String type = DEFAULT_BOOK_TYPE;
+        private Instant reservationDate;
+        private Instant reservationEndDate;
+        private int patronId;
+
+        static BookBuilder newBook() {
+            return new BookBuilder();
+        }
+
+        BookBuilder withId(int bookId) {
+            this.bookId = bookId;
+            return this;
+        }
+
+        BookBuilder thatIsCirculating() {
+            this.type = "CIRCULATING";
+            return this;
+        }
+
+        BookBuilder thatIsRestricted() {
+            this.type = "RESTRICTED";
+            return this;
+        }
+
+        BookBuilder withReservationDate(Instant reservationDate) {
+            this.reservationDate = reservationDate;
+            return this;
+        }
+
+        BookBuilder withReservationEndDate(Instant reservationEndDate) {
+            this.reservationEndDate = reservationEndDate;
+            return this;
+        }
+
+        BookBuilder withPatronId(int patronId) {
+            this.patronId = patronId;
+            return this;
+        }
+
+        Book build() {
+            return new Book(bookId, reservationDate, reservationEndDate, type, patronId);
+        }
+    }
+}
+
