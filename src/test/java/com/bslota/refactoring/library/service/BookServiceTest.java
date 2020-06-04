c9e5fe3f991f61eeb2598d0cf7e516100b8946d9
bslota <bartlomiej.slota@gmail.com>
2020-06-04 15:00:26 +0200

    Object under test created

--- src/test/java/com/bslota/refactoring/library/service/BookServiceTest.java
+++ src/test/java/com/bslota/refactoring/library/service/BookServiceTest.java
@@ -1,13 +1,20 @@
 package com.bslota.refactoring.library.service;
 
+import com.bslota.refactoring.library.dao.BookDAO;
+import com.bslota.refactoring.library.dao.PatronDAO;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 /**
  * @author bslota on 04/06/2020
  */
 class BookServiceTest {
+
+    private BookDAO bookDAO = mock(BookDAO.class);
+    private PatronDAO patronDAO = mock(PatronDAO.class);
+    private NotificationSender notificationSender = mock(NotificationSender.class);
+    private BookService bookService = new BookService(bookDAO, patronDAO, notificationSender);
 
     @Test
     void shouldFailToPlaceNotExistingBookOnHold() {
