c65877f9b275e5a02ef07114a7ba6865852cc30e
bslota <bartlomiej.slota@gmail.com>
2020-06-04 15:02:39 +0200

    Test for not existing book and patron added

--- src/test/java/com/bslota/refactoring/library/service/BookServiceTest.java
+++ src/test/java/com/bslota/refactoring/library/service/BookServiceTest.java
@@ -4,12 +4,17 @@ import com.bslota.refactoring.library.dao.BookDAO;
 import com.bslota.refactoring.library.dao.PatronDAO;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
 
 /**
  * @author bslota on 04/06/2020
  */
 class BookServiceTest {
+
+    private static final int ID_OF_NOT_EXISTING_BOOK = 1;
+    private static final int ID_OF_NOT_EXISTING_PATRON = 2;
+    private static final int PERIOD_IN_DAYS = 100;
 
     private BookDAO bookDAO = mock(BookDAO.class);
     private PatronDAO patronDAO = mock(PatronDAO.class);
@@ -18,7 +23,11 @@ class BookServiceTest {
 
     @Test
     void shouldFailToPlaceNotExistingBookOnHold() {
+        //when
+        boolean result = bookService.placeOnHold(ID_OF_NOT_EXISTING_BOOK, ID_OF_NOT_EXISTING_PATRON, PERIOD_IN_DAYS);
 
+        //then
+        assertFalse(result);
     }
 
     @Test
