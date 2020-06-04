dcd4336f2b732577c65a81442135e39dfbc2a0ef
bslota <bartlomiej.slota@gmail.com>
2020-06-04 14:05:25 +0200

    Tests to implement

---
+++ src/test/java/com/bslota/refactoring/library/service/BookServiceTest.java
@@ -0,0 +1,31 @@
+package com.bslota.refactoring.library.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author bslota on 04/06/2020
+ */
+class BookServiceTest {
+
+    @Test
+    void shouldFailToPlaceNotExistingBookOnHold() {
+
+    }
+
+    @Test
+    void shouldFailToPlaceBookOnHoldWhenPatronHasMaxNumberOfHolds() {
+
+    }
+
+    @Test
+    void shouldFailToPlaceBookOnHoldWhenItIsNotAvailable() {
+
+    }
+
+    @Test
+    void shouldSucceedToPlaceBookOnHold() {
+
+    }
+}
