fc83acc36478f3260dc07fe47b8adc4dffa1630e
bslota <bartlomiej.slota@gmail.com>
2020-06-06 00:30:33 +0200

    MailUtils removed

--- src/main/java/com/bslota/refactoring/library/util/MailDetailsFactory.java
+++ src/main/java/com/bslota/refactoring/library/util/MailDetailsFactory.java
@@ -1,14 +1,19 @@
 package com.bslota.refactoring.library.util;
 
 import com.bslota.refactoring.library.model.PatronLoyalties;
-import com.bslota.refactoring.library.util.MailDetails;
-import com.bslota.refactoring.library.util.MailUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
 
 @Component
 public class MailDetailsFactory {
 
     public MailDetails getFreeBookRewardNotificationFor(PatronLoyalties patronLoyalties) {
-        return MailUtils.freeBookRewardNotificationFor(patronLoyalties);
+        String title = "[REWARD] Patron for free book reward waiting";
+        String body = "Dear Colleague, \n" +
+                "One of our patrons with ID " + patronLoyalties.getPatronId() + " gathered " + patronLoyalties.getPoints() + ". \n" +
+                "Please contact him and prepare a free book reward!";
+        String employees = "customerservice@your-library.com";
+        return MailDetails.from(title, body, Collections.singletonList(employees));
     }
 }
