21eba5378f6c5dde09aa174068cbafb0a8e70a27
bslota <bartlomiej.slota@gmail.com>
2020-06-05 09:35:00 +0200

    First PatronId test added

--- src/test/java/com/bslota/refactoring/library/model/PatronIdTest.java
+++ src/test/java/com/bslota/refactoring/library/model/PatronIdTest.java
@@ -2,13 +2,22 @@ package com.bslota.refactoring.library.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 /**
  * @author bslota on 05/06/2020
  */
 class PatronIdTest {
 
+    public static final int SOME_VALUE = 10;
+
     @Test
     void shouldBeCreatedFromInt() {
+        //when
+        PatronId id = PatronId.of(SOME_VALUE);
+
+        //then
+        assertNotNull(id);
     }
 
     @Test
