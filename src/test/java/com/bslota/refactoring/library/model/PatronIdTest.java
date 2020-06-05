a7ed1863f0c672a74c6e1f358c1aa52175c4fc31
bslota <bartlomiej.slota@gmail.com>
2020-06-05 09:33:32 +0200

    PatronId empty tests added

---
+++ src/test/java/com/bslota/refactoring/library/model/PatronIdTest.java
@@ -0,0 +1,21 @@
+package com.bslota.refactoring.library.model;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bslota on 05/06/2020
+ */
+class PatronIdTest {
+
+    @Test
+    void shouldBeCreatedFromInt() {
+    }
+
+    @Test
+    void shouldBeConvertedToInt() {
+    }
+
+    @Test
+    void twoPatronIdsWithTheSameValueShouldBeEqual() {
+    }
+}
