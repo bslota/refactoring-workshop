02a321fc7a14d5e26115a409faec37db89b243cc
bslota <bartlomiej.slota@gmail.com>
2020-06-05 21:58:26 +0200

    PatronLoyaltiesCalculation empty tests added

---
+++ src/test/java/com/bslota/refactoring/library/service/PatronLoyaltiesCalculationTest.java
@@ -0,0 +1,37 @@
+package com.bslota.refactoring.library.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author bslota on 05/06/2020
+ */
+class PatronLoyaltiesCalculationTest {
+
+    @Test
+    void shouldNotChangeNumberOfPointsWhenPatronTypeIsUnknown() {
+
+    }
+
+    @Test
+    void shouldAddPointsWhenPatronTypeIsRegular() {
+
+    }
+
+    @Test
+    void shouldSetHundredPointsWhenPatronTypeIsPremiumAndThereWereNoPointsBefore() {
+
+    }
+
+    @Test
+    void shouldDoubleThePointsWhenPatronTypeIsPremiumAndThereWereSomePointsBefore() {
+
+    }
+
+    @Test
+    void shouldSetQualifiesForFreeBookFlagWhenLimitIsReached() {
+
+    }
+
+}
