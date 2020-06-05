31b8bacd588f74387ab43d43312e49a43d5e372c
bslota <bartlomiej.slota@gmail.com>
2020-06-06 01:29:10 +0200

    BookService and BookServiceTest adjusted to use BookRepository

--- src/main/java/com/bslota/refactoring/library/service/BookService.java
+++ src/main/java/com/bslota/refactoring/library/service/BookService.java
@@ -1,10 +1,10 @@
 package com.bslota.refactoring.library.service;
 
-import com.bslota.refactoring.library.dao.BookDAO;
 import com.bslota.refactoring.library.dao.PatronDAO;
 import com.bslota.refactoring.library.model.Book;
 import com.bslota.refactoring.library.model.BookId;
 import com.bslota.refactoring.library.model.BookPlacedOnHold;
+import com.bslota.refactoring.library.model.BookRepository;
 import com.bslota.refactoring.library.model.Patron;
 import com.bslota.refactoring.library.model.PatronLoyalties;
 import com.bslota.refactoring.library.model.PlaceOnHoldResult;
@@ -12,30 +12,32 @@ import com.bslota.refactoring.library.util.MailDetails;
 import com.bslota.refactoring.library.util.MailDetailsFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class BookService {
 
-    private final BookDAO bookDAO;
+    private final BookRepository bookRepository;
     private final PatronDAO patronDAO;
     private final NotificationSender emailService;
     private final MailDetailsFactory mailDetailsFactory;
 
-    BookService(BookDAO bookDAO, PatronDAO patronDAO, NotificationSender emailService, MailDetailsFactory mailDetailsFactory) {
-        this.bookDAO = bookDAO;
+    BookService(BookRepository bookRepository, PatronDAO patronDAO, NotificationSender emailService, MailDetailsFactory mailDetailsFactory) {
+        this.bookRepository = bookRepository;
         this.patronDAO = patronDAO;
         this.emailService = emailService;
         this.mailDetailsFactory = mailDetailsFactory;
     }
 
     boolean placeOnHold(int bookId, int patronId, int days) {
-        Book book = bookDAO.getBookFromDatabase(bookId);
+        Optional<Book> book = bookRepository.findBy(BookId.of(bookId));
         Patron patron = patronDAO.getPatronFromDatabase(patronId);
         boolean flag = false;
-        if (thereIsA(book) && thereIsA(patron)) {
-            PlaceOnHoldResult result = patron.placeOnHold(book);
+        if (book.isPresent() && thereIsA(patron)) {
+            PlaceOnHoldResult result = patron.placeOnHold(book.get());
             if (result instanceof BookPlacedOnHold) {
-                book.placedOnHold(patron.getPatronId(), days);
-                bookDAO.update(book);
+                book.get().placedOnHold(patron.getPatronId(), days);
+                bookRepository.update(book.get());
                 patronDAO.update(patron);
                 flag = true;
             }
--- src/test/java/com/bslota/refactoring/library/service/BookServiceTest.java
+++ src/test/java/com/bslota/refactoring/library/service/BookServiceTest.java
@@ -1,13 +1,15 @@
 package com.bslota.refactoring.library.service;
 
-import com.bslota.refactoring.library.dao.BookDAO;
 import com.bslota.refactoring.library.dao.PatronDAO;
 import com.bslota.refactoring.library.fixture.BookFixture;
 import com.bslota.refactoring.library.fixture.PatronFixture;
 import com.bslota.refactoring.library.model.Book;
+import com.bslota.refactoring.library.model.BookRepository;
 import com.bslota.refactoring.library.model.Patron;
 import com.bslota.refactoring.library.util.MailDetailsFactory;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -27,10 +29,10 @@ class BookServiceTest {
     private static final int ID_OF_NOT_EXISTING_PATRON = 2;
     private static final int PERIOD_IN_DAYS = 100;
 
-    private BookDAO bookDAO = mock(BookDAO.class);
+    private BookRepository bookRepository = mock(BookRepository.class);
     private PatronDAO patronDAO = mock(PatronDAO.class);
     private NotificationSender notificationSender = mock(NotificationSender.class);
-    private BookService bookService = new BookService(bookDAO, patronDAO, notificationSender, new MailDetailsFactory());
+    private BookService bookService = new BookService(bookRepository, patronDAO, notificationSender, new MailDetailsFactory());
 
     @Test
     void shouldFailToPlaceNotExistingBookOnHold() {
@@ -86,7 +88,7 @@ class BookServiceTest {
         bookService.placeOnHold(ID_OF_NOT_EXISTING_BOOK, ID_OF_NOT_EXISTING_PATRON, PERIOD_IN_DAYS);
 
         //then
-        verify(bookDAO, never()).update(any());
+        verify(bookRepository, never()).update(any());
         verify(patronDAO, never()).update(any());
         verify(notificationSender, never()).sendMail(any(), any(), any(), any());
     }
@@ -101,7 +103,7 @@ class BookServiceTest {
         bookService.placeOnHold(book.getBookId().asInt(), patron.getPatronId().asInt(), PERIOD_IN_DAYS);
 
         //then
-        verify(bookDAO, atLeastOnce()).update(any());
+        verify(bookRepository, atLeastOnce()).update(any());
         verify(patronDAO, atLeastOnce()).update(any());
         verify(notificationSender, never()).sendMail(any(), any(), any(), any());
     }
@@ -121,13 +123,13 @@ class BookServiceTest {
 
     private Book availableBook() {
         Book book = BookFixture.availableBook();
-        when(bookDAO.getBookFromDatabase(book.getBookId().asInt())).thenReturn(book);
+        when(bookRepository.findBy(book.getBookId())).thenReturn(Optional.of(book));
         return book;
     }
 
     private Book unavailableBook() {
         Book book = BookFixture.unavailableBook();
-        when(bookDAO.getBookFromDatabase(book.getBookId().asInt())).thenReturn(book);
+        when(bookRepository.findBy(book.getBookId())).thenReturn(Optional.of(book));
         return book;
     }
 
