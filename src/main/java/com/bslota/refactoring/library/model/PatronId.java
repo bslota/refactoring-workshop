2cab01afec6936c5378abb82cf3ba007487cc99a
bslota <bartlomiej.slota@gmail.com>
2020-06-05 09:37:59 +0200

    Creator method added

---
+++ src/main/java/com/bslota/refactoring/library/model/PatronId.java
@@ -0,0 +1,11 @@
+package com.bslota.refactoring.library.model;
+
+/**
+ * @author bslota on 05/06/2020
+ */
+class PatronId {
+
+    static PatronId of(int i) {
+        return new PatronId();
+    }
+}
